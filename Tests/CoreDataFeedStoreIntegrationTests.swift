a172247a567163e3a7dfea648601b0b4663f0e4f
Bogdan Poplauschi <bpoplauschi@gmail.com>
2021-01-31 20:09:54 +0200

    Retrieve delivers feed inserted on another instance

--- Tests/CoreDataFeedStoreIntegrationTests.swift
+++ Tests/CoreDataFeedStoreIntegrationTests.swift
@@ -26,14 +26,14 @@ class CoreDataFeedStoreIntegrationTests: XCTestCase {
 	}
 	
 	func test_retrieve_deliversFeedInsertedOnAnotherInstance() {
-		//        let storeToInsert = makeSUT()
-		//        let storeToLoad = makeSUT()
-		//        let feed = uniqueImageFeed()
-		//        let timestamp = Date()
-		//
-		//        insert((feed, timestamp), to: storeToInsert)
-		//
-		//        expect(storeToLoad, toRetrieve: .found(feed: feed, timestamp: timestamp))
+		let storeToInsert = makeSUT()
+		let storeToLoad = makeSUT()
+		let feed = uniqueImageFeed()
+		let timestamp = Date()
+
+		insert((feed, timestamp), to: storeToInsert)
+
+		expect(storeToLoad, toRetrieve: .found(feed: feed, timestamp: timestamp))
 	}
 	
 	func test_insert_overridesFeedInsertedOnAnotherInstance() {
