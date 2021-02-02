528d014ebaacdefb67554c6c0ed83a285c2009f2
Bogdan Poplauschi <bpoplauschi@gmail.com>
2021-02-02 19:59:54 +0200

    Renamed `mapToStorableFeedImage` to `populate` to clarify intent. Also obey the CQS (pass the `CDFeedImage` instance as param)

--- FeedStoreChallenge/CoreData/CoreDataFeedHelper.swift
+++ FeedStoreChallenge/CoreData/CoreDataFeedHelper.swift
@@ -12,25 +12,24 @@ import CoreData
 internal final class CoreDataFeedHelper {
 	internal static func insert(feed: [LocalFeedImage], timestamp: Date, in context: NSManagedObjectContext) {
 		let result = CDFeed(context: context)
-		result.feed = NSOrderedSet(array: feed.map { mapToStorableFeedImage($0, in: context) })
+		result.feed = NSOrderedSet(array: feed.map { populate(storableFeedImage: CDFeedImage(context: context), with: $0) })
 		result.timestamp = timestamp
 	}
 	
 	internal static func mapToFeed(_ cacheFeed: CDFeed) -> (feed: [LocalFeedImage], timestamp: Date) {
-		let feed = cacheFeed.feed.compactMap({ mapToFeedImage($0 as? CDFeedImage) })
+		let feed = cacheFeed.feed.compactMap({   mapToFeedImage($0 as? CDFeedImage) })
 		return (feed, cacheFeed.timestamp)
 	}
 	
 	// MARK: - Helpers
 	
-	private static func mapToStorableFeedImage(_ feedImage: LocalFeedImage, in context: NSManagedObjectContext) -> CDFeedImage {
-		let result = CDFeedImage(context: context)
-		result.id = feedImage.id
-		result.imageDescription = feedImage.description
-		result.imageLocation = feedImage.location
-		result.url = feedImage.url
+	private static func populate(storableFeedImage: CDFeedImage, with feedImage: LocalFeedImage) -> CDFeedImage {
+		storableFeedImage.id = feedImage.id
+		storableFeedImage.imageDescription = feedImage.description
+		storableFeedImage.imageLocation = feedImage.location
+		storableFeedImage.url = feedImage.url
 		
-		return result
+		return storableFeedImage
 	}
 	
 	private static func mapToFeedImage(_ cacheFeedImage: CDFeedImage?) -> LocalFeedImage? {
