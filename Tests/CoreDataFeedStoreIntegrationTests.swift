e4bad67e3d07c8f6bb86be5779a2b81dd3b186e5
Bogdan Poplauschi <bpoplauschi@gmail.com>
2021-02-02 20:14:38 +0200

    Per review - switched back CoreDataFeedStoreIntegrationTests to use a real store url, so we can actually test how the CoreData store works.

--- Tests/CoreDataFeedStoreIntegrationTests.swift
+++ Tests/CoreDataFeedStoreIntegrationTests.swift
@@ -7,6 +7,18 @@ import FeedStoreChallenge
 
 class CoreDataFeedStoreIntegrationTests: XCTestCase {
 	
+	override func setUp() {
+		super.setUp()
+		
+		setupEmptyStoreState()
+	}
+	
+	override func tearDown() {
+		super.tearDown()
+		
+		undoStoreSideEffects()
+	}
+	
 	func test_retrieve_deliversEmptyOnEmptyCache() {
 		let sut = makeSUT()
 
@@ -53,8 +65,28 @@ class CoreDataFeedStoreIntegrationTests: XCTestCase {
 	// - MARK: Helpers
 	
 	private func makeSUT() -> FeedStore {
-		let sut = try! CoreDataFeedStore(storeURL: URL(fileURLWithPath: "/dev/null").appendingPathComponent("shared"))
+		let sut = try! CoreDataFeedStore(storeURL: testSpecificStoreURL())
 		trackForMemoryLeaks(sut)
 		return sut
+	}
+	
+	private func setupEmptyStoreState() {
+		deleteStoreArtifacts()
+	}
+	
+	private func undoStoreSideEffects() {
+		deleteStoreArtifacts()
+	}
+	
+	private func deleteStoreArtifacts() {
+		try? FileManager.default.removeItem(at: testSpecificStoreURL())
+	}
+	
+	private func testSpecificStoreURL() -> URL {
+		return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+	}
+	
+	private func cachesDirectory() -> URL {
+		return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
 	}
 }
